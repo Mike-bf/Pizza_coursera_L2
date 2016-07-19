7fd93a6841ab171b7bd00ce73e0103b039cb4aa6
Mike-bf <miguexz_163@hotmail.com>
2016-07-19 12:41:06 -0600

    Initial Commit

---
+++ Pizza2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pizza2
+//
+//  Created by Miguel angel Bogarin frias on 19/07/16.
+//  Copyright © 2016 MayanTecnology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
